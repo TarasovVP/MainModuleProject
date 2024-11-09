094375837f4293689784f9f82166a836b7c43d8f
TarasovVP <vptarasov1998@gmail.com>
2024-11-09 11:30:18 +0200

    Fix compose test issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,32 @@ rootProject.name = "MainSubmoduleProject"
 include(":mainapp")
 include(":core")
 include(":inner")
+/*var submodulesUpdated = false
+if (!submodulesUpdated) {
+    if (!file("SubModuleProject").exists()) {
+        println("SubModuleProject directory not found. Cloning...")
+        val cloneResult = exec {
+            commandLine("git", "clone", "--recurse-submodules", "https://github.com/TarasovVP/SubModuleProject.git", "SubModuleProject")
+        }.exitValue
+        if (cloneResult != 0) {
+            throw GradleException("Failed to clone SubModuleProject")
+        } else {
+            println("SubModuleProject cloned successfully")
+        }
+    } else {
+        println("SubModuleProject directory found. Updating...")
+        val updateResult = exec {
+            workingDir("SubModuleProject")
+            commandLine("git", "submodule", "update", "--remote", "--merge", "--force")
+        }.exitValue
+        if (updateResult != 0) {
+            throw GradleException("Failed to update SubModuleProject")
+        } else {
+            println("SubModuleProject updated successfully")
+        }
+    }
+    submodulesUpdated = true
+}*/
 include(":submoduleproject")
 project(":submoduleproject").projectDir = file("$rootDir/submoduleproject/app")
  
