87421e00266fd88d54630ccdbb67de8acece7402
TarasovVP <vptarasov1998@gmail.com>
2024-11-10 11:16:55 +0200

    Change ci yml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,44 +23,5 @@ rootProject.name = "MainSubmoduleProject"
 include(":mainapp")
 include(":inner")
 
-/*var submodulesUpdated = false
-if (!submodulesUpdated) {
-    val submodulePath = file("SubModuleProject")
-    if (submodulePath.exists()) {
-        println("SubModuleProject exists.")
-        submodulePath.delete()
-        println("SubModuleProject is deleted.")
-    } else {
-        println("SubModuleProject exists.")
-    }
-    val updateResult = exec {
-        commandLine("git", "submodule", "update", "--recursive", "--remote")
-    }.exitValue
-
-    if (updateResult != 0) {
-        throw GradleException("Failed to update SubModuleProject")
-    } else {
-        println("SubModuleProject updated successfully.")
-    }
-    repeat(5) { attempt ->
-        if (submodulePath.exists() && submodulePath.list()?.isNotEmpty() == true) {
-            println("SubModuleProject is fully updated and ready.")
-            return@repeat 
-        } else {
-            println("Waiting for SubModuleProject to be ready... Attempt ${attempt + 1}/5")
-            Thread.sleep(5000)
-        }
-    }
-
-    if (submodulePath.list()?.isEmpty() == true) {
-        throw GradleException("SubModuleProject update did not complete as expected.")
-    } else {
-        println("SubModuleProject is fully updated and ready(final).")
-    }
-    include(":submoduleproject")
-    project(":submoduleproject").projectDir = file("$rootDir/submoduleproject/app")
-    submodulesUpdated = true
-}*/
-
 include(":submoduleproject")
 project(":submoduleproject").projectDir = file("$rootDir/submoduleproject/app")
